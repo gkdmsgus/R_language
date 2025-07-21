66e4b46f166a5ea45079c12c82e4c0b682bfc6bf
gkdmsgus <gkdmsgus0422@gmail.com>
2025-07-21 11:33:20 +0900

    2025/07/21 am

--- 25.07.21/07.21/chap7.R
+++ 25.07.21/07.21/chap7.R
@@ -1,3 +1,4 @@
+rm(list = ls())
 #7-10
 head(iris)
 order(iris$Sepal.Length)
@@ -29,3 +30,51 @@ subset(iris, Sepal.Length > 5.1 &
          Sepal.Length >3.9)
 subset(iris, Sepal.Length > 7.6,
          select=c(Petal.Length,Petal.Width))
+iris[1,]
+iris[1]
+iris[c(1,2)]
+iris[colnames(iris)[1:2]]
+iris[colnames(iris)[3:4]]
+subset(iris, Sepal.Length > 7.6)[colnames(iris)[3:4]]
+
+#7-13
+x <- 1:100
+y <- sample(x,size = 10, replace = FALSE)
+y
+
+#############연습#############
+sample(1:10,10)
+sample(1:5,6)
+sample(1:5, 7, replace = TRUE)
+##############################
+
+#7-14
+idx <- smaple(1:nrow(iris), size=50,
+              replace = FALSE)
+idx
+iris.50 <- iris[idx,]
+dim(iris.50)
+head(iris.50)
+
+#7-15
+sample(1:20, size = 5)
+sample(1:20, size = 5)
+sample(1:20, size = 5)
+
+set.seed(100)
+sample(1:20, size = 5)
+set.seed(100)
+sample(1:20, size = 5)
+set.seed(100)
+sample(1:20, size = 5)
+
+if(!require(dplyr)){
+  install.packages("dplyr")
+}
+library(dplyr)
+
+??sample_n
+sample_n(iris,10)
+sample_frac(iris)
+sample_frac(iris, .1)
+sample_frac(iris, .2)
