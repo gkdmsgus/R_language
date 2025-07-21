f4528e5d0bb8432a6b7cef1766608c8b87312581
gkdmsgus <gkdmsgus0422@gmail.com>
2025-07-21 11:24:55 +0900

    2025/07/21 am

--- 25.07.21/07.21/chap7.R
+++ 25.07.21/07.21/chap7.R
@@ -10,4 +10,22 @@ iris[order(iris$Species,-iris$Petal.Length,decreasing = T),]
 sp <- split(iris, iris$Species)
 sp
 summary(sp)
+str(sp)
+class(sp)
+typeof(sp)
+mode(sp)
+
 sp$setosa
+sp$virginica
+sp$versicolor
+sp[[1]]
+sp[[2]]
+sp[[3]]
+
+#7-12
+subset(iris, Species == "setosa")
+subset(iris, Sepal.Length > 7.5)
+subset(iris, Sepal.Length > 5.1 &
+         Sepal.Length >3.9)
+subset(iris, Sepal.Length > 7.6,
+         select=c(Petal.Length,Petal.Width))
