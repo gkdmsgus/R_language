b190b7f0ec10c743c44c6eb7d886113c25a3b757
gkdmsgus <gkdmsgus0422@gmail.com>
2025-07-21 10:44:31 +0900

    2025/07/21

--- 25.07.21/07.21/chap7.R
+++ 25.07.21/07.21/chap7.R
@@ -6,3 +6,8 @@ iris[order(iris$Sepal.Length, decreasing = T),]
 iris.new <- iris[order(iris$Sepal.Length),]
 head(iris.new)
 iris[order(iris$Species,-iris$Petal.Length,decreasing = T),]
+#7-11
+sp <- split(iris, iris$Species)
+sp
+summary(sp)
+sp$setosa
